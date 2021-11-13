ef383d92db6dd8598679e47258750693b9108214
Кирилл Глебов <kirillglebov.51@mail.ru>
2021-11-13 23:51:18 +0300

    update_html_js_css

--- wb_hackathon/js/script.js
+++ wb_hackathon/js/script.js
@@ -30,10 +30,35 @@ $(document).ready(function() {
     };
   });
 
+  jQuery(list_element).each(function() {
+    var link_on_li = this;
+    var link_on_tag = this.children;
+    // показ блока и описания тега
+    $(link_on_tag).mouseover(function() {
+      var attr = $(link_on_li).attr("data-tooltip"); // копируем содержимое data-tooltip. Если ничего нет (или не сущ-т) - undefined
+      if(attr) {
+        $(".show-descr").css('left', $(link_on_li).offset().left);
+        $(".show-descr").css('top', $(link_on_li).offset().top + 25);
+        $(".show-descr").css('opacity',  1);
+        $(".show-descr").html($(link_on_li).attr('data-tooltip'));
+      };
+    });
+    // сокрытие блока описания при уведении мышки от тега
+    $(link_on_tag).mouseout(function(){
+      $(".show-descr").css('opacity',  0);
+    });
+  });
 
-  // $(element).slick(){
-  //
-  // };
+  // работа слайдера
+  $('.single-item').slick({
+     variableWidth: true,
+     infinite: true,
+     dots: false,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     prevArrow: "<img src='./img/vector-left.png' alt='left-arrow' class='prev'>",
+     nextArrow: "<img src='./img/vector-right.png' alt='right-arrow' class='next'>",
+   });
 
 
 });
